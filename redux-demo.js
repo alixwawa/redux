9aa1101a773b79d93c7a8f06d7b751c635d3567c
Alix Wawa <alixwawa@yahoo.com>
2023-05-11 15:27:50 -0500

    using dispatch

--- redux-demo.js
+++ redux-demo.js
@@ -3,9 +3,19 @@ const redux = require('redux');
 //reducer
 //must give state a initial state. in this example state = {counter: 0}
 const counterReducer = (state = { counter: 0 }, action) => {
-	return {
-		counter: state.counter + 1,
-	};
+	if (action.type === 'increment') {
+		return {
+			counter: state.counter + 1,
+		};
+    }
+    
+	if (action.type === 'decrement') {
+		return {
+			counter: state.counter - 1,
+		};
+	}
+
+	return state;
 };
 
 //store with reducer
@@ -14,11 +24,16 @@ const store = redux.createStore(counterReducer);
 //subscriber
 const counterSubscriber = () => {
 	const latestState = store.getState();
-	console.log(latestState);
+	console.log('hi', latestState);
 };
 
 store.subscribe(counterSubscriber);
 
+//dispatch an action
 store.dispatch({
 	type: 'increment',
+});
+
+store.dispatch({
+	type: 'decrement',
 });
